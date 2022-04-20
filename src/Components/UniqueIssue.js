f18a7d7c53feeb62989377e60f92e49a939b10f0
sandeepshakya2019 <sandeepshakya2015@gmail.com>
2022-04-21 01:26:53 +0530

    Second COmmit

--- src/Components/UniqueIssue.js
+++ src/Components/UniqueIssue.js
@@ -9,24 +9,36 @@ function UniqueIssue({ issuenumber, data }) {
           if (d.number === issuenumber) {
             return (
               <>
-                <h1>
+                <h1 className="head-title container-fluid">
                   #{d.number} {d.title}
                 </h1>
-                <h3>{d.url}</h3>
-                <h3>{d.repository_url}</h3>
-                <p>ID : {d.id}</p>
-                <p>State : {d.state}</p>
-                <p>Created At : {d.created_at}</p>
-                <p>Updated At : {d.updated_at}</p>
-                <p>Closed At : {d.closed_at}</p>
-                <h3>Body of the issue</h3>
-                <div dangerouslySetInnerHTML={{ __html: d.body }} />
-                <h4>User Information</h4>
-                <a href={d.user.html_url}>
-                  <p>User Login : {d.user.login}</p>
-                </a>
-                <p>User ID : {d.user.id}</p>
-                <p>User Avatar URL : {d.user.avatar_url}</p>
+                <div className="container">
+                  <br />
+                  <h4 className="headings">Users Related URL</h4>
+                  <br />
+
+                  <h6 className="head-url">{d.url}</h6>
+                  <h6 className="head-url">{d.repository_url}</h6>
+                  <br />
+
+                  <h4 className="headings">Issues Realted Data</h4>
+                  <br />
+
+                  <p>ID : {d.id}</p>
+                  <p>State : {d.state}</p>
+                  <p>Created At : {d.created_at}</p>
+                  <p>Updated At : {d.updated_at}</p>
+                  <p>Closed At : {d.closed_at}</p>
+                  <h4 className="headings">Body of the issue</h4>
+                  <br />
+                  <dir>{d.body}</dir>
+                  <h4 className="headings">User Information</h4>
+                  <a href={d.user.html_url}>
+                    <p>User Login : {d.user.login}</p>
+                  </a>
+                  <p>User ID : {d.user.id}</p>
+                  <p>User Avatar URL : {d.user.avatar_url}</p>
+                </div>
               </>
             );
           }
